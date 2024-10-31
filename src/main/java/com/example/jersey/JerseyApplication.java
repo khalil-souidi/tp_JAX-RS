68642ac8500c7dcfa298b62e78bb7ca179d6154b
khalilsouidi <khalilsouidi9@gmail.com>
2024-10-31 22:41:16 +0100

    first commit

---
+++ src/main/java/com/example/jersey/JerseyApplication.java
@@ -0,0 +1,32 @@
+package com.example.jersey;
+
+import com.example.jersey.entities.Compte;
+import com.example.jersey.entities.TypeCompte;
+import com.example.jersey.repository.CompteRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class JerseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JerseyApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner start(CompteRepository compteRepository) {
+		return args -> {
+			compteRepository.save(new Compte(null, Math.random() * 9000, new Date(), TypeCompte.EPARGNE));
+			compteRepository.save(new Compte(null, Math.random() * 9000, new Date(), TypeCompte.COURANT));
+			compteRepository.save(new Compte(null, Math.random() * 9000, new Date(), TypeCompte.EPARGNE));
+			compteRepository.findAll().forEach(c -> {
+				System.out.println(c.toString());
+			});
+		};
+	}
+
+}
---
+++ src/main/java/com/example/jersey/entities/TypeCompte.java
@@ -0,0 +1,5 @@
+package com.example.jersey.entities;
+
+public enum TypeCompte {
+    COURANT,EPARGNE
+}
